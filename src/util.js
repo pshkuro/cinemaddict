07bb2a45e2e6892a654ad3f9199b3a390af44455
Полина <polina110297@yandex.ru>
2020-04-14 17:02:48 +0300

    Рефакторит функцию рендеринга элементов на страницу

--- src/util.js
+++ src/util.js
@@ -1,3 +1,20 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+// Функция рендеринга DOM-элементов на страницу
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
 // Функция для создания DOM-элемента на основе шаблона разметки
 export const createElement = (template) => {
   const newElement = document.createElement(`div`);
