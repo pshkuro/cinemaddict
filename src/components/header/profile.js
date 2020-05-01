82acc227abca3346e45d540cc8234279eb39a69c
Полина <polina110297@yandex.ru>
2020-05-01 14:20:12 +0300

    Add ProfileRating

--- src/components/header/profile.js
+++ src/components/header/profile.js
@@ -1,11 +1,29 @@
 import AbstractComponent from "../abstract-component";
+import {ProfileRatingRules, FilterType} from "../../const";
+import {getFilmsByFilter} from "../../utils/filters";
 
 // Генерация Звания пользователя
 export default class ProfileComponent extends AbstractComponent {
+  constructor(films) {
+    super();
+    this._films = films;
+  }
+
+  getProfileRating() {
+    const watchedFilmsCount = getFilmsByFilter(this._films, FilterType.HISTORY).length;
+
+    const ratingRule = ProfileRatingRules.find((rule) => {
+      return rule.from <= watchedFilmsCount && watchedFilmsCount <= rule.to;
+    });
+
+    return ratingRule.rating;
+
+  }
+
   getTemplate() {
     return (
       `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
+      <p class="profile__rating">${this.getProfileRating()}</p>
       <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
     );
--- src/main.js
+++ src/main.js
@@ -9,15 +9,17 @@ import FilmsModel from "./models/films";
 
 const CARD_FILM_COUNT = 12;
 
+const films = generateCards(CARD_FILM_COUNT); // Массив объектов карточек кол-ом CARD_FILM_COUNT
+const filmsModel = new FilmsModel();
+filmsModel.setFilms(films);
+
+
 const headerPageElement = document.querySelector(`header`);
-render(headerPageElement, new ProfileComponent(), RenderPosition.BEFOREEND);
+render(headerPageElement, new ProfileComponent(filmsModel.getAllFilms()), RenderPosition.BEFOREEND);
 
 export const mainPageElement = document.querySelector(`.main`);
 const footerStatisticsElement = document.querySelector(`.footer__statistics`);
 
-const films = generateCards(CARD_FILM_COUNT); // Массив объектов карточек кол-ом CARD_FILM_COUNT
-const filmsModel = new FilmsModel();
-filmsModel.setFilms(films);
 
 const filtersController = new FiltersController(mainPageElement, filmsModel);
 filtersController.render();
